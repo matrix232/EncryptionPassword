0fc14d2c62983bf8394c456cca28bf21e9883b6b
matrix232 <egortop15@gmail.com>
2022-12-01 13:50:25 +0300

    Encryption

---
+++ Encryption/Encryption.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    internal class EncryptionPassword
+    {
+        protected char[] Password { get; set; }
+        private int Key { get; set; }
+        
+        public EncryptionPassword(char[] password)
+        {
+            Password = password;
+            Key = 4;
+        }
+
+        private void OriginalPassword()
+        {
+            Console.Write("[+] Original password: ");
+            for (int i = 0; i < Password.Length; i++)
+            {
+                Console.Write(Password[i].ToString());
+            }
+            Console.WriteLine();
+        }
+
+        public void Encode()
+        {
+            int LengthPassword = Password.Length;
+            int[] EncodePassword = new int[LengthPassword];
+            char Accept = ' ';
+            Console.WriteLine("[?] Do you want to watch original password, yes[+] or no[-]?");
+            Accept = Convert.ToChar(Console.ReadLine());
+            if (Accept == '+')
+            {
+                OriginalPassword();
+            }
+            else
+            {
+                Console.WriteLine("[-] OK");
+            }
+
+            for (int i = 0; i < Password.Length; i++)
+            {
+                EncodePassword[i] = (int)Password[i] + Key;
+            }
+
+            Console.Write("[ENCRYPTION] Your encryption password: ");
+            for(int i = 0; i < Password.Length; i++)
+            {
+                Console.Write(((char)EncodePassword[i]).ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encryption
+{
+    class Programm
+    {
+        static void Main(string[] argc)
+        {
+            Console.Write("Enter a password: ");
+            string StringPassword = Console.ReadLine();
+            char[] passwordChars = StringPassword.ToCharArray();
+
+            EncryptionPassword encryption = new EncryptionPassword(passwordChars);
+            encryption.Encode();
+        }
+    }
+}
